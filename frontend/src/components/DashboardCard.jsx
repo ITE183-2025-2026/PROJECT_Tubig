4865605ce4a581b2359ba9042fa2588df574373d
Cidndrw <159919693+AndrewCid@users.noreply.github.com>
2026-01-04 22:01:17 +0800

    hiding some parts :>

--- frontend/src/components/DashboardCard.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/DashboardCard.jsx
-export default function DashboardCard({ title, children }) {
-  return (
-    <div className="bg-[#141414] p-6 border border-gray-800 rounded-2xl shadow-lg hover:shadow-xl transition">
-      <h3 className="text-lg font-semibold mb-3">{title}</h3>
-      <div className="text-gray-300">{children}</div>
-    </div>
-  );
-}
